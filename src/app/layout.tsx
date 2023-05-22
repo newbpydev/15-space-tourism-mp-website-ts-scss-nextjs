561936b1aa157e51032a144152d837cd481fb95b
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-05-21 21:33:14 -0300

    feat: add favicon.png to public directory feat: add global styles in globals.scss for consistent styling across the app refactor: change file extension of globals.css to globals.scss refactor: update layout.tsx to import globals.scss instead of globals.css refactor: update page.tsx to import page.module.scss instead of page.module.css refactor: update class names in page.module.scss to use kebab-case instead of camelCase for consistency refactor: update Home component in page.tsx to use kebab-case class names from page.module.scss for consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import "./globals.css";
+import "./globals.scss";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
   title: "Frontend Mentor | Space Tourism Website",
   description:
     "Explore the wonders of Space Tourism on our captivating website. Embark on a celestial journey through stunning visuals and immersive experiences. Join us in making dreams of space travel a reality. #webdevelopment #frontend #spacetourism #frontendmentor",
-  icons: [{ rel: "icon", url: "./favicon.png" }],
+  icons: [{ rel: "icon", url: "./favicon.png", sizes: "any" }],
 };
 
 export default function RootLayout({
