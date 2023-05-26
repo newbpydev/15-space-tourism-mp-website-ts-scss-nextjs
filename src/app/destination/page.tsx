ada56a7535bf9b2ba6f1624fe6908ac4eff96cea
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-05-26 18:42:53 -0300

    feat(destination): add support for fetching destination data from data.json file using getStaticProps
    feat(destination): add Destination interface to types to improve type safety
    
    style(destination.module.scss): import variables.scss to use variables in styles

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,11 +1,27 @@
+import { GetStaticProps } from "next";
 import styles from "../styles/pages/destination.module.scss";
+import path from "path";
+
+interface Props {}
 
 function DestinationPage() {
   return (
     <main className={styles.main}>
-      <h1>Destination</h1>
+      <h1>Destination!</h1>
     </main>
   );
 }
 
 export default DestinationPage;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const destination = await fetch(
+    path.join(__dirname, "..", "data", "data.json")
+  );
+  console.log(destination);
+  console.log("ji");
+
+  return {
+    props: {},
+  };
+};
---
+++ src/app/types/destination.ts
@@ -0,0 +1,10 @@
+export interface Destination {
+  name: string;
+  images: {
+    png: string;
+    webp: string;
+  };
+  description: string;
+  distance: string;
+  travel: string;
+}
