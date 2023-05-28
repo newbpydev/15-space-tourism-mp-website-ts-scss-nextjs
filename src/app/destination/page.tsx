1bd3f1513df782f2c226e86ebc6657f2baf2e2dc
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-05-27 22:04:23 -0300

    refactor(destination): replace fs.readFile with fs.readFileSync to simplify code and improve readability feat(localdata): add getDestinations function to retrieve destinations data from local file system

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,21 +1,10 @@
-import { GetStaticProps } from "next";
 import styles from "../styles/pages/destination.module.scss";
-import path from "path";
-import fs from "fs";
+import { getAllData, getDestinations } from "../lib/localdata";
 
 interface Props {}
 
 async function DestinationPage() {
-  const url = path.join(process.cwd(), "data/data.json");
-
-  fs.readFile(url, "utf-8", (err, data) => {
-    if (err) {
-      console.error(`Error reading file from disk: ${err}`);
-    } else {
-      const jsonData = JSON.parse(data);
-      console.log(jsonData);
-    }
-  });
+  const destinations = await getDestinations();
 
   return (
     <main className={styles.main}>
--- src/app/lib/localdata.ts
+++ src/app/lib/localdata.ts
@@ -1,1 +1,30 @@
-export const getAllData = () => {};
+import fs from "fs";
+import path from "path";
+import { Destination } from "../types/destination";
+import { Crew } from "../types/crew";
+import { Technology } from "../types/technology";
+
+interface Data {
+  destinations: Destination[];
+  crew: Crew[];
+  technology: Technology[];
+}
+
+export const getAllData = async () => {
+  const url = path.join(process.cwd(), "data/data.json");
+  // let data: Data;
+
+  try {
+    const fsData = fs.readFileSync(url, "utf-8");
+    const jsonData: Data = JSON.parse(fsData);
+    // data = jsonData;
+    return jsonData;
+  } catch (error) {
+    console.error(`Error reading file from disk: ${error}`);
+  }
+};
+
+export const getDestinations = async () => {
+  const data = await getAllData();
+  if (data) return data.destinations;
+};
