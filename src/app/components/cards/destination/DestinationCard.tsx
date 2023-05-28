6482e52f0a5bbf95db748dfe6819f4ac643e138c
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2023-05-28 20:48:15 -0300

    style(DestinationCard.tsx): add styles to DestinationCard component to improve visual hierarchy and readability

--- src/app/components/cards/destination/DestinationCard.tsx
+++ src/app/components/cards/destination/DestinationCard.tsx
@@ -1,6 +1,7 @@
 import { Destination } from "@/app/types/destination";
 import React from "react";
 import DestinationNavBar from "../../navbar/DestinationNavBar";
+import styles from "../../../styles/components/cards/destination/DestinationCard.module.scss";
 
 interface Props {
   destination: Destination;
@@ -8,19 +9,19 @@ interface Props {
 
 function DestinationCard({ destination }: Props) {
   return (
-    <article>
-      <header>
+    <article className={styles.card}>
+      <header className={styles.header}>
         <DestinationNavBar />
-        <h2>{destination.name}</h2>
+        <h2 className={styles.title}>{destination.name}</h2>
       </header>
 
-      <p>{destination.description}</p>
+      <p className={styles.description}>{destination.description}</p>
 
-      <footer>
-        <p>
+      <footer className={styles.footer}>
+        <p className={styles.distance}>
           Avg. Distance <span>{destination.distance}</span>
         </p>
-        <p>
+        <p className={styles.travel}>
           Est. Travel Time <span>{destination.travel}</span>
         </p>
       </footer>
